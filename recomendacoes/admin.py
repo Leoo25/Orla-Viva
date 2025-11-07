e721de00bb554c16e4f343797547a5dbce8f5e6a
rickyyn <ritsccd@gmail.com>
2025-11-07 00:26:19 -0300

    Atualizacao

---
+++ recomendacoes/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Recomendacao, CategoriaRecomendacao
+
+
+@admin.register(Recomendacao)
+class RecomendacaoAdmin(admin.ModelAdmin):
+    
+    list_display = ('nome', 'descricao', 'latitude', 'longitude','redeSocial_url')
+    
+    fieldsets = (
+        ('Informações Principais', {
+            'fields': ('nome', 'descricao', 'telefone','redeSocial_url','categoria','faixa_preco','endereco_texto','imagem')
+        }),
+        ('Localização (Clique no mapa para definir)', {
+            'description': "Use o mapa abaixo para definir a localização. Você pode clicar ou arrastar o marcador.",
+            
+            'fields': ('latitude', 'longitude') 
+        }),
+    )
+    class Media:
+    
+        css = {
+            'all': ('https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',)
+        }
+
+        js = (
+            'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+            'admin/js/admin_map.js',
+        )
+@admin.register(CategoriaRecomendacao)
+class CategoriaRecomendacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    fieldsets = (('Informações',{
+        'fields':('nome','cor_pin','icone_pin','imagem')
+    }),)
