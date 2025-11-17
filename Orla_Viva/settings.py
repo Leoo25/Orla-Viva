0ca5b9ece431528252d8c2954cfb95138f61586e
Leo <mendoncaleonardo547@gmail.com>
2025-11-16 23:08:56 -0300

    Adicionando informações na Página Eventos

--- Orla_Viva/settings.py
+++ Orla_Viva/settings.py
@@ -72,7 +72,7 @@ JAZZMIN_SETTINGS = {
 }
 
 TAILWIND_APP_NAME = 'theme'
-NPM_BIN_PATH = r"D:\Gordo\Node\npm.cmd"
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 
 MIDDLEWARE = [
@@ -113,7 +113,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'orla_viva_db',   
         'USER': 'postgres',      
-        'PASSWORD': 'gordo78', 
+        'PASSWORD': '200502', 
         'HOST': 'localhost',  
         'PORT': '5432',       
 
