9eb77eeb13fd166a853763252eeb1c0476ca208c
rickyyn <ritsccd@gmail.com>
2025-12-01 20:43:56 -0300

    Arrumando o menu inicial

--- theme/recomendacoes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from .views import recomendacoes, lista_por_categoria
-
-urlpatterns = [
-    path('recomendacoes/',recomendacoes,name='recomendacoes'),
-    path('categoria/<int:pk>/',lista_por_categoria, name='lista_recomendacoes_por_categoria'),
-]
